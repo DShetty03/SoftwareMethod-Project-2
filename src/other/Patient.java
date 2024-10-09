4ef810356f637ce2a27afb7b3bde3a3caa1660c0
Divit Shetty <divitshetty@divits-mbp-2.mynetworksettings.com>
2024-10-09 00:27:32 -0400

    Fixed Patient

--- src/other/Patient.java
+++ src/other/Patient.java
@@ -21,6 +21,28 @@ public class Patient extends Person {
     }
 
     /**
+     * Adds a new visit to the patient's list of visits.
+     * This method maintains a linked list of visits, ensuring
+     * that the most recent visit is added at the end of the list.
+     *
+     * @param newVisit The visit to be added.
+     *                 If it's the first visit, it will be set as the head of the list.
+     *                 If there are existing visits, it will be appended at the end.
+     */
+    public void addVisit(Visit newVisit) {
+        if (visit == null) {
+            visit = newVisit;
+        } else {
+            Visit current = visit;
+            // Traverse till the last visit in the list
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newVisit);  // Link the new visit at the end
+        }
+    }
+
+    /**
      * Get the visit information for the patient.
      *
      * @return The visit associated with this patient.
@@ -29,14 +51,18 @@ public class Patient extends Person {
         return this.visit;
     }
 
+
     /**
      * Set the visit information for the patient.
      *
      * @param visit The visit to be associated with this patient.
      */
+    //Set visit might be unnecessary so commenting it out
+    /*
     public void setVisit(Visit visit) {
         this.visit = visit;
     }
+    */
 
     /**
      * Check if two Patient objects are equal based on their profile.
